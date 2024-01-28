ad965ad09a8a3f5ba7b80d577a15763dacd3e9c2
David Molla <davidmquinteiro@gmail.com>
2024-01-28 15:24:02 +0100

    Changed paths for fine exec wit azfunction

--- ApiInstatoDF/compute.py
+++ ApiInstatoDF/compute.py
@@ -1,27 +1,34 @@
-import sys
-sys.path.append('/Users/davidmolla/Data/Formacion/MBIT School/00 TFM/TrendAi_repo')
+# import sys
+import logging
+from azure.functions import HttpRequest, HttpResponse
+from datetime import datetime
+
+# sys.path.append('/Users/davidmolla/Data/Formacion/MBIT School/00 TFM/TrendAi_repo')
 #### ---- IMPORTS ---- ####
 from pathlib import Path  # noqa: E402
 
-from ApiInstatoDF import INGEST_DATE, create_df, get_urls_to_df    # noqa: E402
+from .ApiInstatoDF import create_df, get_urls_to_df  # noqa: E402
 
 # from common.utils import connect_to_s3, upload_to_s3
 from common.utils import connect_to_s3, upload_to_s3  # noqa: E402
 
 #### ---- GLOBAL VARIABLES ---- ####
+INGEST_DATE = datetime.now().strftime("%Y-%m-%d %H:%M")
 _EXPORT_PATH = Path(__file__).parent / "data"
-BUCKET_NAME = 'trendai-bucket-01'
+BUCKET_NAME = "trendai-bucket-01"
 _ROOT = Path(__file__).parent.parent
-s3 = connect_to_s3(_ROOT, json_file="boto3keys.local.json")
 
 ### ---- MAIN COMPUTE FUNCION ---- ###
 
-def compute():
+
+def main(req: HttpRequest) -> HttpResponse:
+    req_body = req.get_json()
     """
     Main function to run the program.
     """
-    NAME = input("Enter the name of the profile: ")
-    LEN = int(input("Enter the number of images to scrape: "))
+    s3 = connect_to_s3(_ROOT, json_file="boto3keys.local.json")
+    NAME = req_body.get("name")
+    LEN = req_body.get("len")
     df_data = get_urls_to_df(NAME, LEN)
     df = create_df(df_data)
     # Save the DataFrame to a csv file.
@@ -30,11 +37,15 @@ def compute():
     try:
         upload_to_s3(s3, BUCKET_NAME, df, f"{INGEST_DATE[:7]}-{NAME}.csv")
     except Exception as e:
-        print(f"--ERROR UPLOADING TO S3--\n{e}")
-    
+        logging.info(f"--ERROR UPLOADING TO S3--\n{e}")
 
-    print(df.head(5))
+    return HttpResponse(
+        f"""Downloaded {len(df)} images from {NAME}.
+        Ingested {INGEST_DATE}.
+        Uploaded to S3 bucket {BUCKET_NAME}.""",
+        status_code=200,
+    )
 
 
-if __name__ == "__main__":
-    compute()
+# if __name__ == "__main__":
+#     main()
