65ddf11544bab2f5543c041518264f4ff2336146
David Molla <davidmquinteiro@gmail.com>
2024-03-01 14:09:47 +0100

    Actualizar función get_image_responses para utilizar un dataframe en lugar de listas separadas de imágenes, diseñadores, temporadas y años

--- GPT_Vision/lambda_function_df.py
+++ GPT_Vision/lambda_function_df.py
@@ -9,13 +9,10 @@ project_path = "/Users/davidmolla/Data/Formacion/MBIT School/00 TFM/TrendAi_repo
 sys.path.append(project_path)
 from common.utils import load_gpt_files, initialize_openai, save_gpt_response_txt # FIX: import from common.utils  
 
-
-
-def get_image_responses(client, image_descipcion_prompt, IMAGES: dict, _ROOT: str,  DESIGNER, TEMPORADA, YEAR) -> list:
+def get_image_responses(client, image_descipcion_prompt, df, _ROOT: str) -> list:
     responses_to_df = []
 
-    # for image in IMAGES["Images"].values():
-    for image, designer, temporada, year, ranGE in zip(IMAGES, DESIGNER, TEMPORADA, YEAR, range(len(IMAGES))):
+    for index, row in df.iterrows():
         response = client.chat.completions.create(
             model="gpt-4-vision-preview",
             messages=[
@@ -26,7 +23,7 @@ def get_image_responses(client, image_descipcion_prompt, IMAGES: dict, _ROOT: st
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": image,
+                                "url": row["url"],
                                 "detail": "low"
                             },
                         },
@@ -44,11 +41,16 @@ def get_image_responses(client, image_descipcion_prompt, IMAGES: dict, _ROOT: st
             file.unlink()
 
         for choice in response.choices:
-            data = {"timestamp": pd.Timestamp.today().strftime("%Y_%m_%d_%H_%M"),
-                    "image_response": choice.message.content,
-                    "url": image}
+            data = {
+                "timestamp": pd.Timestamp.today().strftime("%Y_%m_%d_%H_%M"),
+                "image_response": choice.message.content,
+                "url": row["url"],
+                "designer": row["designer"],
+                "temporada": row["temporada"],
+                "year": row["year"]
+            }
             responses_to_df.append(data)
-            print(ranGE, choice.message.content[:50])
+            print(index, choice.message.content[:50])
 
     return responses_to_df
 
@@ -61,17 +63,12 @@ def lambda_handler(event, context):
     client = initialize_openai("OPENAI_API_KEY")
     df = pd.read_csv("vogue_season_2_reduced.csv")
     image_descipcion_prompt = load_gpt_files(_ROOT, "txt")
-    # IMAGES = load_gpt_files(_ROOT, "json")
-    IMAGES = df["url"].tolist()
-    DESIGNER = df["designer"].tolist()
-    TEMPORADA = df["temporada"].tolist()
-    YEAR = df["year"].tolist()
 
-    LIST = get_image_responses(client, image_descipcion_prompt, IMAGES, _ROOT, DESIGNER, TEMPORADA, YEAR)
-    df = get_df_from_list(LIST)
+    LIST = get_image_responses(client, image_descipcion_prompt, df, _ROOT)
+    df_result = get_df_from_list(LIST)
 
     csv_filename = f"{_ROOT}/image_responses/image_responses_{pd.Timestamp.today().strftime('%Y_%m_%d_%H_%M')}.csv"
-    df.to_csv(csv_filename, index=False)
+    df_result.to_csv(csv_filename, index=False)
 
     return {
         'statusCode': 200,
