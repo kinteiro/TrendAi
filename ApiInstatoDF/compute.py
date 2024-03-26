5b4aff279e88f40961775ab7fb582acc7d9c594c
David Molla <davidmquinteiro@gmail.com>
2024-03-26 14:40:13 +0100

    Updating the S3 connection

--- ApiInstatoDF/compute.py
+++ ApiInstatoDF/compute.py
@@ -2,17 +2,15 @@ import sys
 sys.path.append('/Users/davidmolla/Data/Formacion/MBIT School/00 TFM/TrendAi_repo')
 #### ---- IMPORTS ---- ####
 from pathlib import Path  # noqa: E402
-
 from ApiInstatoDF import INGEST_DATE, create_df, get_urls_to_df    # noqa: E402
-
-# from common.utils import connect_to_s3, upload_to_s3
-from common.utils import connect_to_s3, upload_to_s3  # noqa: E402
+from common.s3_service import S3Connector
 
 #### ---- GLOBAL VARIABLES ---- ####
 _EXPORT_PATH = Path(__file__).parent / "data"
 BUCKET_NAME = 'trendai-bucket-01'
 _ROOT = Path(__file__).parent.parent
-s3 = connect_to_s3(_ROOT, json_file="boto3keys.local.json")
+s3 = S3Connector(_ROOT)
+# s3 = connect_to_s3(_ROOT, json_file="boto3keys.local.json")
 
 ### ---- MAIN COMPUTE FUNCION ---- ###
 
@@ -28,7 +26,7 @@ def compute():
     df.to_csv(f"{_EXPORT_PATH/INGEST_DATE[:7]}-{NAME}.csv", index=False)
     # Upload the DataFrame to S3.
     try:
-        upload_to_s3(s3, BUCKET_NAME, df, f"{INGEST_DATE[:7]}-{NAME}.csv")
+        s3.upload_df_to_s3(BUCKET_NAME, df, f"{INGEST_DATE[:7]}-{NAME}.csv")
     except Exception as e:
         print(f"--ERROR UPLOADING TO S3--\n{e}")
     
