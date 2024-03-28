190dd3a0742c93fbd864a8da4cb8152a16150ebd
David Molla <davidmquinteiro@gmail.com>
2024-03-28 21:22:09 +0100

    Delete unnecessary __init__.py file in GPT_Vision directory

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# GPT_Vision/__init__.py
-
-# Este archivo puede estar vacío o contener cualquier configuración necesaria.
-# Su presencia convierte el directorio GPT_Vision en un paquete reconocido por Python.
